6e46a13c362fb9f510f1e683cba5bdd088b38edc
priyanshu-code <rawatpriyanshu007@gmail.com>
2023-03-08 18:52:10 +0530

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const {register,login} = require("../controllers/auth")
 router.route('/login').post(login)
 router.route('/register').post(register)
-
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
 module.exports = router
